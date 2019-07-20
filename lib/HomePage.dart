5e6f13a6d990fc167029422c8f46472d79c6fb9c
Jaybhuke <Jaybhuke97@gmail.com>
2019-07-20 12:25:33 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class HomePage extends StatefulWidget {
+  final String title;
+  HomePage({
+    Key key,
+    @required
+    this.title
+  }) : super(key: key);
+
+  @override
+   _HomePageState createState() => _HomePageState();
+}
+class _HomePageState extends State<HomePage> {
+  final String url="https://api.github.com/users";
+  List data;
+  var isLoading=false;
+
+  @override
+  void initState(){
+    super.initState();
+    this.getjsondata();
+  }
+
+  Future<String> getjsondata() async{
+    var response = await http.get(
+      Uri.encodeFull(url),
+    );
+    // print(response.body);
+    setState(() {
+     var convertDatatoJson= json.decode(response.body);
+     data=convertDatatoJson;
+     print(data);
+    });
+  }
+
+   @override
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemCount: data!=null?data.length:0,
+        itemBuilder: (BuildContext context, int index) {
+          Card(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('Dummy Data',
+                  style: TextStyle(
+                    fontSize: 20.0
+                  ),
+                  ),
+                  subtitle: Text('Sub Dummy text',
+                  style:TextStyle(
+                    fontSize:15.0
+                  )
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      )
+    );
+  }
+}
+
+
+
+
+
+
